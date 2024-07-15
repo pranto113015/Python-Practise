b2040fed94d64dfa3a1685f89e9068728fe2d190
pranto113015 <pranto3876@gmail.com>
2024-07-15 21:02:15 +0600

    add class 2 code

---
+++ class-2.py
@@ -0,0 +1,56 @@
+# add two number
+a = 10
+b = 5
+sum = a+b
+print("The sum of two number :",sum)
+
+# sub two number
+a = 10
+b = 5
+sub = a-b
+print("The sub of two number :",sub)
+
+# Multiplication two number
+a = 10
+b = 3
+mul = a*b
+print("The multiplication of two number :",mul)
+
+# Division  two number
+a = 45
+b = 5
+div = a/b
+print("The answer of 45/5 is :",div)
+
+
+#program for adding three no
+X = 12
+Y = 8
+Z = 5
+sum = X+Y+Z
+print("The sum of three number :",sum)
+
+
+# Find the area of triangle
+a = 6
+b = 7
+s= 0.5*a*b
+print("The Area of Triangle : ",s)
+
+
+# Area of rectangle
+width = 8
+height =6
+rectangle_area = width*height
+print("Area of Rectangle is : ", rectangle_area)
+
+
+
+# area of circle
+pi = 3.1416
+r = 6
+area = pi*r*r
+print("The area of circle is :" ,area)
+
+
+
