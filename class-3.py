b3315e08f4540b0913bb724a8778198e45789012
pranto113015 <pranto3876@gmail.com>
2024-08-03 11:57:36 +0600

    add class 2 code

---
+++ class-3.py
@@ -0,0 +1,56 @@
+# Python Arithmetic Operators
+
+# Modulus Operator
+a = 50
+b = 3
+modulus = a%b
+print('The Modulus Result Is :',modulus)
+
+# Exponentiation Operator
+a = 2
+b = 5
+exponent = a** b
+print('The Exponentiation Result Is :',exponent)
+
+# Floor division Operator
+a = 15
+b = 2
+floorDivision = a//b
+print('The Floor Division Result Is :',floorDivision)
+
+
+
+
+# Python Comparison Operators
+
+# Equal Operator
+x = 5
+y = 3
+print(x == y)
+
+# Greater than or equal to
+x = 5
+y = 3
+print(x >= y)
+
+# Less than or equal to
+x = 5
+y = 3
+print(x <= y)
+
+
+
+
+# Python Logical Operators
+
+#  and operator
+x = 5
+print(x > 3 and x < 10)
+
+# or operator
+x = 5
+print(x > 3 or x < 4)
+
+# not operator
+x = 5
+print(not(x > 3 and x < 10))
